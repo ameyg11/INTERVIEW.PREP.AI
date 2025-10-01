25a38d45f1ec1790850fb38f2ff9170f9c8b3430
Amey <ameygawade11@gmail.com>
2025-10-01 14:38:57 +0530

    created a login and get user profile API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -49,12 +49,47 @@ const registerUser = async (req, res) => {
 // @route   POST /api/auth/login
 // @access  Public
 const loginUser = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if(!user){
+            return res.status(500).json({ message: "Invalid email or password" });
+        }
+        console.log(user);
+
+        // Compare password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            return res.status(500).json({ message: "Invalid email or password" });
+        }
+
+        // Return user with data
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            profileImageUrl: user.profileImageUrl,
+            token: generateToken(user._id),
+        });
+    }catch(err){
+         res.status(500).json({ message: "Server Error", err: err.message });
+    }
 };
 
 // @desc    Get user profile
 // @route   POST /api/auth/profile
 // @access  Private (Requires JWT)
 const getUserProfile = async (req, res) => {
+    try{
+        const user = await User.findById(req.user.id).select("-password");
+        if(!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        res.json(user);
+    }catch(err){
+         res.status(500).json({ message: "Server Error", err: err.message });
+    }
 };
 
 
